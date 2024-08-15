64ac9ee607167484b2832454fb20cc39ccb7f343
Ilikepinapples <willschofield08@gmail.com>
2024-08-16 00:48:08 +0100

    incremental for mixin tests

--- src/main/java/entity0/coppernetworks/copperNetworkPowerAPI.java
+++ src/main/java/entity0/coppernetworks/copperNetworkPowerAPI.java
@@ -1,7 +1,5 @@
 package entity0.coppernetworks;
 
 public interface copperNetworkPowerAPI {
-    default CopperNetworkPowerClass copperNetworkAPI() {
-        return null;
-    }
+    CopperNetworkPowerClass copperNetworkAPI();
 }
