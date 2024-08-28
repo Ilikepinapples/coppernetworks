27c024833761baf3ab925f065c3daa0d11494eaf
Ilikepinapples <willschofield08@gmail.com>
2024-08-29 00:55:33 +0100

    Fix item networking

--- src/main/java/entity0/coppernetworks/copperNetworkItemPowerClass.java
+++ src/main/java/entity0/coppernetworks/copperNetworkItemPowerClass.java
@@ -16,15 +16,20 @@ public class copperNetworkItemPowerClass {
 
         public int getNetworkID() {return id;}
         public int id;
-        public long networkMaxPower = ((long) NetworkerClass.Networks.get(id).get(1));
+        public long getNetworkMaxPower (int id) {
+            if (!(NetworkerClass.Networks.get(id) == null)) {
+                return (long) NetworkerClass.Networks.get(id).get(1);
+            }
+            else {return 0L;}
+        }
 
 
 
         public boolean canConsume(long consumePower) {return consumePower <= NetworkerClass.getPower(id);}
-        public boolean canGenerate(long genPower) {return genPower + NetworkerClass.getPower(id) <= networkMaxPower;}
+        public boolean canGenerate(long genPower) {return genPower + NetworkerClass.getPower(id) <= getNetworkMaxPower(id);}
         public void generate(long genPower) {NetworkerClass.setPower((NetworkerClass.getPower(id) + genPower), id);}
         public void consume(long consumePower) {NetworkerClass.setPower((NetworkerClass.getPower(id) - consumePower), id);}
-        public void setNetworkmaxPower() {NetworkerClass.setPower(networkMaxPower, id);}
+        public void setNetworkmaxPower() {NetworkerClass.setPower(getNetworkMaxPower(id), id);}
         public void cleanupNetwork() {
             id = 0;
         }
